4143b403dec3492db0b607fb9e7f60cd6bcb8454
Katerina <katerinaafadeeva@gmail.com>
2024-01-31 22:30:41 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+export const env = {
+  browser: true,
+  es2021: true,
+};
+export const overrides = [];
+export const parserOptions = {
+  ecmaVersion: 'latest',
+  sourceType: 'module',
+};
+export const rules = {};
