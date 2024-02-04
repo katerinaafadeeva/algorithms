db0efcafae5fcd5186b1d5f68aca659fa6620160
Katerina Fadeeva <fadeeva@myservices.digital>
2024-02-04 17:51:08 +0300

    solved

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function findTheSmallestInteger(str) {
-    var arrFromStr = str.split("");
-    console.log(arrFromStr);
-}
-console.log(findTheSmallestInteger("17181920"));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
+
 function findTheSmallestInteger(str: string){
-const arrFromStr = str.split("")
-console.log(arrFromStr)
+
+    for (let i = 0; i < str.length; i++) {
+      let currentNumber = parseInt(str.slice(0, i + 1));
+      let nextExpected = currentNumber + 1;
+      let sequence = currentNumber.toString()
+
+
+      while (sequence.length < str.length) {
+        sequence += nextExpected;
+        nextExpected++;
+    }
+
+    if(sequence===str){
+      return currentNumber
+    }
+    }
 }
 
-
-console.log(findTheSmallestInteger("17181920" ))
+console.log(findTheSmallestInteger("121122123"))
+console.log(findTheSmallestInteger("9899100"));
